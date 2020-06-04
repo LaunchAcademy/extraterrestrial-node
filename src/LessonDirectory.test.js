42d323bdaebc6bcb2013f4af47a82b6ba7ae24ae
Cassandra Newell <cassandra.newell@protonmail.com>
2020-06-04 07:59:27 -0400

    fix test failures

--- src/LessonDirectory.test.js
+++ src/LessonDirectory.test.js
@@ -3,14 +3,14 @@ const path = require("path")
 
 describe("Lesson Directory", () => {
   it("returns null if no parent contains .et", async () => {
-    const dir = await LessonDirectory.recursivelyLoad("bin")
-    expect(dir).toEqual(null)
+    const { directory } = await LessonDirectory.recursivelyLoad("bin")
+    expect(directory).toBeUndefined()
   })
 
   it("identifies lesson directory", async () => {
-    const dir = await LessonDirectory.recursivelyLoad(
+    const { directory } = await LessonDirectory.recursivelyLoad(
       "test/fixtures/bloated-challenge/rando_folder/rando_file.js"
     )
-    expect(dir).toEqual("test/fixtures/bloated-challenge")
+    expect(directory).toEqual("test/fixtures/bloated-challenge")
   })
 })
