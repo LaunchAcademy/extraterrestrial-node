f850332f1f3a4083273a1fe048ae29296fea4d6f
Cassandra Newell <cassandra.newell@protonmail.com>
2020-06-04 07:55:28 -0400

    fix bugs

--- src/LessonDirectory.js
+++ src/LessonDirectory.js
@@ -7,19 +7,17 @@ class LessonDirectory {
   static async recursivelyLoad(dir = process.cwd()) {
     if (!this.directory) {
       let pathToCheck = dir
-      while (pathToCheck !== "." && !this.directory) {
+      while (pathToCheck !== "." && pathToCheck !== "/" && !this.directory) {
         const parentDir = path.dirname(pathToCheck)
-
         try {
           await fs.access(parentDir + "/" + configFileName)
-          debugger
           this.directory = pathToCheck
         } catch (error) {
           pathToCheck = parentDir
         }
       }
     }
-    return this.directory || null
+    return this
   }
 }
 
